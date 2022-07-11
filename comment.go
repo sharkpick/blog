c2c1e3dc2249c2d4a306d97309cda2912be1441c
Will Dillon <wmdillon@me.com>
2022-07-11 06:51:37 -0400

    adding more code to manage comments

--- comment.go
+++ comment.go
@@ -24,6 +24,39 @@ func NewComment(postID int64, body, name string) Comment {
 	}
 }
 
+func InsertComment(db *sql.DB, body, name string, postid int64) (Comment, error) {
+	timestamp := time.Now().Format(TimestampFormat)
+	sql_string := "INSERT INTO " + CommentsTable + "(body, name, timestamp, postid) VALUES (?, ?, ?, ?)"
+	prepared, err := db.Prepare(sql_string)
+	if err != nil {
+		return Comment{}, fmt.Errorf("InsertComment %w: %s", ErrUnableToPrepare, err)
+	}
+	n, err := prepared.Exec(body, name, timestamp, postid)
+	if err != nil {
+		return Comment{}, fmt.Errorf("InsertComment %w: %s", ErrUnableToExecute, err)
+	}
+	id, err := n.LastInsertId()
+	if err != nil {
+		return Comment{}, fmt.Errorf("InsertComment %w: %s", ErrCantGetLastInsertID, err)
+	}
+	return GetCommentByID(db, id)
+}
+
+func GetCommentByID(db *sql.DB, id int64) (Comment, error) {
+	sql_string := "SELECT id, body, name, timestamp, postid FROM " + CommentsTable + " WHERE id=?"
+	prepared, err := db.Prepare(sql_string)
+	if err != nil {
+		return Comment{}, fmt.Errorf("GetCommentByID %w: %s", ErrUnableToPrepare, err)
+	}
+	row := prepared.QueryRow(id)
+	var comment Comment
+	err = row.Scan(&comment.ID, &comment.Body, &comment.Name, &comment.Timestamp, &comment.PostID)
+	if err != nil {
+		return Comment{}, fmt.Errorf("GetCommentByID %w: %s", ErrUnableToScan, err)
+	}
+	return comment, nil
+}
+
 func GetComments(db *sql.DB, id int64) ([]Comment, error) {
 	results := make([]Comment, 0)
 	sql_string := "SELECT id, body, name, timestamp, postid FROM " + CommentsTable + " WHERE postid=?"
